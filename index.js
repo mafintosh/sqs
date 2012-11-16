f62213f128e0f2d0863fcd1d8c640ca437218ef5
mafintosh <mathiasbuus@gmail.com>
2012-11-16 20:34:36 +0100

    retry failed failed push and deletes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 var SIGNATURE_METHOD  = 'HmacSHA256';
 var SIGNATURE_VERSION = '2';
-var SIGNATURE_TTL = 30*1000;
+var SIGNATURE_TTL = 150*1000;
 var VERSION = '2012-11-05';
 var DEFAULT_REGION = 'us-east-1';
 
@@ -31,28 +31,28 @@ module.exports = function(options) {
 	options.access = options.access || process.env.SQS_ACCESS_KEY;
 	options.secret = options.secret || process.env.SQS_SECRET_KEY;
 	options.region = options.region || process.env.SQS_REGION || DEFAULT_REGION;
-	options.token  = options.token  || process.env.SQS_TOKEN;
 
 	if (!options.access || !options.secret) throw new Error('options.access and options.secret are required');
 
 	var queues = {};
 	var closed = false;
+	var proto = options.https ? 'https://' : 'http://';
+	var host = 'sqs.'+options.region+'.amazonaws.com';
 	var agent = new http.Agent();
+	var namespace = options.namespace ? options.namespace+'-' : '';
 
-	var req = request.defaults({ // we use long polling so we wanna use over own agent
+	var req = request.defaults({ // we use long polling so we wanna use our own agent
 		agent:agent
 	});
 
 	var queryURL = function(action, path, params) {
-		var host = 'sqs.'+(options.region || DEFAULT_REGION)+'.amazonaws.com';
-
 		params = params || {};
 
 		params.Action = action;
 		params.AWSAccessKeyId = options.access;
 		params.SignatureMethod = SIGNATURE_METHOD;
 		params.SignatureVersion = SIGNATURE_VERSION;
-		params.Expires = new Date(options.expires || Date.now()+SIGNATURE_TTL).toISOString();
+		params.Expires = new Date(Date.now()+SIGNATURE_TTL).toISOString();
 		params.Version = VERSION;
 
 		if (options.token) {
@@ -65,7 +65,21 @@ module.exports = function(options) {
 
 		params.Signature = options.signature || crypto.createHmac('sha256',options.secret).update(stringToSign).digest('base64');
 
-		return 'http://'+host+path+'?'+qs.stringify(params);
+		return proto+host+path+'?'+qs.stringify(params);
+	};
+
+	var retry = function(req, url) {
+		var retries = 0;
+		var action = function() {
+			req(url, function(err, res) {
+				if (!err || res.statusCode < 500) return;
+				retries++;
+				if (retries > 15) return that.emit('error', new Error('could send '+url));
+				setTimeout(action, retries*1000);
+			});
+		};
+
+		action();
 	};
 
 	var queueURL = function(name, callback) {
@@ -92,8 +106,7 @@ module.exports = function(options) {
 		req(queryURL('CreateQueue', '/', {QueueName:name}), function(err) {
 			if (err) return onresult(err);
 			req(queryURL('GetQueueUrl', '/', {QueueName:name}), function(err, res) {
-				if (err) return onresult(err);
-				if (res.statusCode !== 200) return onresult(new Error('could not create queue'));
+				if (err || res.statusCode !== 200) return onresult(err);
 				onresult(null, '/'+text(res.body, 'QueueUrl').split('/').slice(3).join('/'));
 			});
 		});
@@ -102,13 +115,17 @@ module.exports = function(options) {
 	var that = new process.EventEmitter();
 
 	that.push = function(name, message) {
+		name = namespace+name;
+
 		queueURL(name, function(url) {
-			req(queryURL('SendMessage', url, {MessageBody:JSON.stringify(message)})); // TODO: check response and retry if err
+			retry(req, queryURL('SendMessage', url, {MessageBody:JSON.stringify(message)}));
 		});
 	};
 
 	that.pull = function(name, workers, onmessage) {
 		if (typeof workers === 'function') return that.pull(name, options.workers || 1, workers);
+
+		name = namespace+name;
 
 		range(workers).forEach(function() {
 			var next = function() {
@@ -133,7 +150,7 @@ module.exports = function(options) {
 
 						onmessage(body, function(err) {
 							if (err) return next();
-							req(queryURL('DeleteMessage', url, {ReceiptHandle:receipt})); // TODO: check response and retry if err
+							retry(req, queryURL('DeleteMessage', url, {ReceiptHandle:receipt}));
 							next();
 						});
 
